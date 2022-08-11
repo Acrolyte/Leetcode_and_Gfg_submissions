bb893f16c333710a807ed84f01f1b8ca42ce9ea7
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-11 14:53:40 +0530

    Time: 1568 ms (5.17%), Space: 307.8 MB (5.10%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    void putvalue(int n,char a, char b,char c, string &ans){
+        if(n == 0) return;
+        else if(n>=1 && n<=3){
+            while(n--) ans += a;
+        } else if(n == 4){
+            ans += a ;
+            ans += b;
+        } else if(n >= 5 && n < 9){
+            ans += b;
+            n -= 5;
+            while(n--) ans += a;
+        } else {
+            ans += a ;
+            ans += c;
+        }
+    }
+
+    string intToRoman(int num) {
+        int t=0,h=0,te=0,o=0;
+        if(num>=1000){
+            t = num / 1000;
+            num %= 1000;
+        }
+        if(num>=100){
+            h = num/100;
+            num %=100;
+        }
+        if(num>=10){
+            te = num/10;
+            num %= 10;
+        }
+        o = num;
+        string ans = "";
+        while(t--) ans += 'M';
+        putvalue(h,'C','D','M',ans);
+        putvalue(te,'X','L','C',ans);
+        putvalue(o,'I','V','X',ans);
+
+        return ans;    
+    }
+    int romanToInt(string s) {
+        vector<string> v(4000);
+        int ans=0;
+        for(int i=1;i<4000;i++){
+            v[i] = intToRoman(i);
+            if(v[i] == s){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
