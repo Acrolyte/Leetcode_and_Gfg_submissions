bf3a2dea0e39c03312737a419d0c8781812078b5
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-11 14:03:28 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.63%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        long long ans = 0;
+        while(x!=0){
+            ans = ans*10 + x%10;
+            x /= 10;
+        }
+        if(ans < INT_MIN or ans > INT_MAX) return 0;
+        else return (int)ans;
+    }
+};
