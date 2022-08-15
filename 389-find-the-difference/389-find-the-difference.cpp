2e658730224a1959138c043e5c1de259b8c0eb65
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-15 22:44:42 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (68.10%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        s += t;
         char ans = 0;
         for(auto i: s) ans ^= i;
+        for(auto i: t) ans ^= i;
         return ans;
     }
 };
