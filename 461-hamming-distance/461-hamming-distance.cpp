4d487079e66e437c26c4c74cc3d9d85f8ec0d2b9
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-16 20:04:15 +0530

    Time: 6 ms (7.93%), Space: 5.9 MB (77.19%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int c=0;
-        for(int i=0;i<32;i++) if( (x & (1<<i)) != (y & (1<<i)))c++;
-        return c;
+        return __builtin_popcount(x^y);
     }
 };
