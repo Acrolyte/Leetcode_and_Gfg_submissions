5ab4a09560b096a0a58afb62b718922bf733ae8d
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-16 21:14:42 +0530

    Time: 15 ms (56.05%), Space: 9.9 MB (97.30%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+    static bool cmp(int a,int b){
+        int x = bitset<32>(a).count();
+        int y = bitset<32>(b).count();
+        if(x == y) return a <= b;
+        return x <= y;
+    }
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>> v;
-        for(auto i: arr) v.push_back({__builtin_popcount(i),i});
-        sort(begin(v),end(v));
-        vector<int> ans;
-        for(auto i: v){
-            ans.push_back(i.second);
-        }
-        return ans;
+        sort(begin(arr),end(arr),cmp);
+        return arr;
     }
 };
