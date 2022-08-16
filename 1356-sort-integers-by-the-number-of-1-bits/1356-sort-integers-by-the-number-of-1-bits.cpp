0321c028a505f78f48d60049339ab4ebd6c88078
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-16 21:15:57 +0530

    Time: 7 ms (94.27%), Space: 10.2 MB (44.36%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    static bool cmp(int a,int b){
-        int x = bitset<32>(a).count();
-        int y = bitset<32>(b).count();
-        if(x == y) return a <= b;
-        return x <= y;
-    }
+    
     vector<int> sortByBits(vector<int>& arr) {
-        sort(begin(arr),end(arr),cmp);
+        sort(begin(arr),end(arr),[&] (int a, int b){
+            int x = bitset<32>(a).count();
+            int y = bitset<32>(b).count();
+            if(x == y) return a <= b;
+            return x <= y; 
+        });
         return arr;
     }
 };
