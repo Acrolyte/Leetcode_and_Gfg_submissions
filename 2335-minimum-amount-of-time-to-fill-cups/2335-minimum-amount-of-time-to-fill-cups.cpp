95b6ed557cd8d62299541d212386f84e36869b81
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-19 14:57:41 +0530

    Time: 5 ms (51.50%), Space: 11.7 MB (91.97%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     int fillCups(vector<int>& a) {
         int s = a[0]+a[1]+a[2];
-        int c=0;
-        while(s>0){
-            sort(a.begin(),a.end());
-            if(a[2]) a[2]--;
-            if(a[1]) a[1]--;
-            c++;
-            s = a[0]+a[1]+a[2];
-        }
-        return c;
+        int m = max(a[0],max(a[1],a[2]));
+        return max(m,(s+1)/2);
     }
 };
