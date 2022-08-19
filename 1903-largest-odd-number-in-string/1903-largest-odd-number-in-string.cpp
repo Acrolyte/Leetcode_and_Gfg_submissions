c3cb091277036832f7802deadb0947beb70d6549
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-19 16:18:13 +0530

    Time: 50 ms (25.83%), Space: 15 MB (46.20%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string ans ;
-        for(int i=num.length()-1;i>=0;i--){
-            if((num[i]-'0')&1){
-                ans = num.substr(0,i+1);
-                break;
-            }
-        }
-        return ans;        
+        return num.substr(0,num.find_last_of("13579")+1);
     }
 };
