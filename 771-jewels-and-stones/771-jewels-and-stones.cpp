f704a44c27cb0871c5bf707373097107b2a47388
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-08-20 14:05:14 +0530

    Time: 3 ms (63.52%), Space: 6.6 MB (7.27%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int numJewelsInStones(string j, string stones) {
-		unordered_set<char> J(j.begin(),j.end());
-		int ans = 0;
-		for(auto i: stones) if(J.count(i)) ans++;
-		return ans;
+    int numJewelsInStones(string jewels, string stones) {
+        map<char,int> mp;
+        for(auto i: stones) mp[i]++;
+        int ans = 0;
+        for(auto i: jewels) if(mp[i]!=0) ans += mp[i];
+        return ans;
     }
 };
