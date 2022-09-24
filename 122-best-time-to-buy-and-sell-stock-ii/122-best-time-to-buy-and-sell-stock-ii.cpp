59b77d5220cfd7f6c7148ac3494cbd043151a952
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-09-24 15:21:42 +0530

    Time: 15 ms (43.20%), Space: 13 MB (90.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,18 @@
 class Solution {
-private:
-    int maxProfitRec(vector<int>&prices, int i, bool can,vector<vector<int>>&ds){
-        int n = prices.size();
-        if(i >= n) return 0;
-        
-        if(ds[i][can] != -1) return ds[i][can];
-        
-        if(can){
-            int notBuy = maxProfitRec(prices,i+1,can,ds);
-            int buy = maxProfitRec(prices,i+1,!can,ds) - prices[i];
-            return ds[i][can] = max(notBuy,buy);
-        }
-        else {
-             int notSell = maxProfitRec(prices,i+1,can,ds);
-            int sell = maxProfitRec(prices,i+1,!can,ds) + prices[i];
-            return ds[i][can] = max(notSell,sell);
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        if(n<=1) return 0;
-        vector<vector<int>> ds(n+1, vector<int>(2,-1));
-        return maxProfitRec(prices,0,true,ds);
+        int buy = 0, sell = 0, i = 0, ans = 0;
+        
+        while(i < n-1){
+            while(i<n-1 && prices[i] >= prices[i+1])
+                i++;
+            buy = prices[i];
+            while(i<n-1 && prices[i] < prices[i+1])
+                i++;
+            sell = prices[i];
+            ans += sell - buy;
+        }
+        return ans;
     }
 };
