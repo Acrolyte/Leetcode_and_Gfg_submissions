5328c9c5b79825712c9d63f98114c4086153447d
Arpit Chaurasia <59145196+Acrolyte@users.noreply.github.com>
2022-10-02 18:53:19 +0530

    Time: 291 ms (7.05%), Space: 46.8 MB (15.76%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        map<int,int> mp;
+        int c = 0, el = INT_MAX;
+        for(auto i: nums){
+            if(i%2==0) mp[i]++;
+            
+            if(mp[i]>c or (mp[i]== c and i<el) ){
+                c = mp[i];
+                el = i;
+            }
+        }
+        if(c == 0) return -1;
+        return el;
+    }
+};
